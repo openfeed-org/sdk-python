49405382c0f5bc1a7bba146cdf5980c25e9c7f0d
Mike <mvberg@gmail.com>
2020-08-25 10:54:03 -0500

    latest

--- openfeed/__init__.py
+++ openfeed/__init__.py
@@ -1,3 +1,11 @@
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent)) 
+
+# https://github.com/protocolbuffers/protobuf/issues/1491#issuecomment-547504972
+# FIXME
+
 from .openfeed_client import OpenfeedClient
 
-VERSION = '1.0.0'
+VERSION = '1.0.2'
