8d2ebc0a6bc282ff703ad769a696a9c4e00d63a9
Mike <mvberg@gmail.com>
2021-01-04 13:20:31 -0600

    v1.1.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='openfeed',
-    version='1.1.0',
+    version='1.1.1',
     author='Barchart',
     author_email='mike@barchart.com',
     license='MIT',
