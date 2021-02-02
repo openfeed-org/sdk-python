d9cffceed937dd8aa7eef6ab87511d570c7326f3
mvberg <mvberg@gmail.com>
2021-02-02 12:32:55 -0600

    symbol callback

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='openfeed',
-    version='1.1.3',
+    version='1.1.5',
     author='Barchart',
     author_email='mike@barchart.com',
     license='MIT',
